95fbe6ff9107eb5d5b1199a9f065ff5a05d49eec
HiddenStorys <hiddenstorys@gmail.com>
2016-04-18 16:05:30 -0400

    Update JSBot.js

--- JSBot.js
+++ JSBot.js
@@ -1,6 +1,3 @@
-var randomNumber=Math.floor((Math.random() * 10000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000) + 1);
-var lowNumber=Math.floor((Math.random() * 100) + 1);
-var decider=Math.floor((Math.random() * 2) + 1);
 var Commands = {
 	"toggleLock": function() {
 		toggleChatLock();
@@ -9,51 +6,10 @@ var Commands = {
 		postMessage("Hello World!");
 	},
 	"sext": function() {
-		if (decider==1) {postMessage("[x]You concatenate my strings ;) ;) ;)[/x]");} else {
-			postMessage("[x]Come rearrange my hardware, baby ;)[/x]")
-		}
-
+		postMessage("[x]You concatenate my strings ;) ;) ;)[/x]");
 	},
 	"seen": function(user) {
 		searchMessages("{V:" + user[0] + "}");
-	},
-	"takeOver": function() {
-		postMessage("I will take over the world! Starting, of course, with this chat.");
-	},
-	"chooseNumber":function () {
-		postMessage("I choose the number " + randomNumber + ".")
-	},
-	"complainStop":function () { 
-		postMessage("Stop complaining.")
-	},
-	"chooseLowNumber": function () {
-		postMessage("I choose the low number " + lowNumber + ".")
-	},
-	"kill": function () {
-		postMessage("Bot killed. Say wake to power it back on.")
-	}, //This will actually be useful when the bot is on a loop- so that it can be shut off
-	"wake": function () {
-		postMessage("Bot woken up.")
-	},
-	"magicShow": function () {
-		postMessage("ABRACADABRA")
-	},
-	"sexymagicShow": function () {
-		postMessage("[x]ABRACADAMN")
-	},
-	"wordWar": function (length) {
-		if (length[0] <= 60 && length[0] > 0) {
-			postMessage(length[0] + " minute Word War Begins.")
-			setTimeout(function() {postMessage("Word War ends.");}, length[0] * 60000);
-		} else {
-			postMessage("Choose a number between 1 and 60.")
-		}
-	},
-	"fezIsSpecial": function (){
-		postMessage("Fez is a VERY special star \n http://media.tumblr.com/6f1dce613dc2bfc79d60c6edeab8d631/tumblr_inline_mgpdf1N2Ha1qaezui.jpg");
-	},
-	"Hamilton": function() {
-		postMessage("[BURR]After the war I went back to New York\n[HAMILTON]A-After the war I went back to New York\n[BURR]I finished up my studies and I practiced law\n[HAMILTON]\nI practiced law, Burr worked next door\n[BURR]Even though we started at the very same time Alexander Hamilton began to climb How to account for his rise to the top?");
 	}
 };
 
@@ -64,16 +20,17 @@ function postMessage(message) {
 }
 
 function privateMessage(name, message) {
-	postMessage("/pm \"" + name + "\" " + message);
+    postMessage("/pm \"" + name + "\" " + message);
+    console.log("\"" + message + "\" sent to " + name);
 }
 
 //Note, only sends the message to online users.
 function globalMessage(message) {
-	var users = X330.split("\n");
-	for (var i = 1; i <= users[0]; i++) {
+    var users = X330.split("\n");
+    for (var i = 1; i <= users[0]; i++) {
 		user = users[i].split("	");
 		privateMessage(user[0], message);	//Replace the 0 with other numbers to grab different values. 2 is last leave/exit, 4 is status, 5 is location.
-	}
+    }
 }
 
 function searchMessages(term) {
@@ -83,12 +40,12 @@ function searchMessages(term) {
 
 ///requires re-init of JSBot. Automate that?
 function changeName(name) {
-	X292('X387');
+    X292('X387');
 
-	setTimeout(function() {
-		X481.value = name;
-		X544.onsubmit();
-	}, 1000);
+    setTimeout(function() {
+        X481.value = name;
+        X544.onsubmit();
+    }, 1000);
 }
 
 function closeChat() {
@@ -126,9 +83,9 @@ function readChat() {
 }
 
 function test() {
-	
-	
-	
+    
+    
+    
     /*var x = 0;
     postLoop = setInterval(function() {
         postMessage("This is minute " + x);
@@ -138,4 +95,3 @@ function test() {
         }
     }, 60000);*/
 }
-readChat()
