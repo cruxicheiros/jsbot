90c36fdd113b285a623df4b45a4ad725e8dc342f
Rune Tynan <runetynan@gmail.com>
2016-08-12 22:38:35 -0400

    Stripped down for pull back to original JSBot

--- Talos.js
+++ Talos.js
@@ -5,11 +5,10 @@
 */
 
 //Constants
-const VERSION = 1.2;
+const VERSION = 0.0;
 const BOOT_TIME = new Date();
-const WH_TIME = 0;
+const SCHEDULED_TIME = 0;
 const ADMINS = ["Dino", "α|CraftSpider|Ω", "HiddenStorys"];
-const EGG_DEV = "wundrweapon" //this is just here so wundrweapon feels good about himself
 
 //Chatzy Variables
 var messageTable = "X8414";
@@ -18,25 +17,11 @@ var messageTime = "X2087";
 var messageButton = "X5794"
 
 //Command variables
-var NumWWs = 0;
-var MaxWWs = 10;
 var IsSleeping = 0;
 
 //Writing Hour variables
-var WHActive = false;
-var WHDisactive = false;
-var WHAlertOne = false;
-var WHAlertTwo = false;
-
-//Generator words
-var Noun = ["dog", "cat", "robot", "astronaut", "man", "woman", "person", "child", "giant", "elephant", "zebra", "animal", "box", "tree", "wizard", "mage", "swordsman", "soldier", "inventor", "doctor", "Talos", "dinosaur", "insect", "nerd", "dancer", "singer", "actor", "barista", "acrobat", "gamer", "writer", "dragon"];
-var Adjective = ["happy", "sad", "athletic", "giant", "tiny", "smart", "silly", "unintelligent", "funny", "coffee-loving", "lazy", "spray-tanned", "angry", "disheveled", "annoying", "loud", "quiet", "shy", "extroverted", "jumpy", "ditzy", "strong", "weak", "smiley", "annoyed", "dextrous"];
-var Goal = ["fly around the world", "go on a date", "win a race", "tell their crush how they feel", "find their soulmate", "write a chatbot", "get into university", "graduate high school", "plant a hundred trees", "find their biological parents", "fill their bucket list", "find atlantis", "learn magic", "learn to paint", "drive a car", "pilot a spaceship", "leave Earth", "go home", "redo elementary school", "not spill their beer"];
-var Obstacle = ["learning to read", "fighting aliens", "saving the world", "doing algebra", "losing their hearing", "losing their sense of sight", "learning the language", "hacking the mainframe", "coming of age", "the nuclear apocalypse is happening", "incredibly drunk", "drinking coffee", "surfing", "spying on the bad guys", "smelling terrible", "having a bad hair day", "exploring the new planet", "on the moon", "on Mars"];
-
-var Place = ["pub", "spaceship", "museum", "office", "jungle", "forest", "coffee shop", "store", "market", "station", "tree", "hut", "house", "bed", "bus", "car", "dormitory", "school", "desert", "ballroom", "cattery", "shelter", "street"];
-var Place_Adjective = ["quiet", "loud", "crowded", "deserted", "bookish", "colorful", "balloon-filled", "book", "tree", "money", "video game", "cat", "dog", "busy", "apocalypse", "writer", "magic", "light", "dark", "robotic", "futuristic", "old-timey"];
-var Action = ["learn to read", "jump up and down", "cry a lot", "cry a little", "smile", "spin in a circle", "get arrested", "dance to the music", "listen to your favourite song", "eat all the food", "win the lottery", "hack the mainframe", "save the world", "find atlantis", "get accepted to Hogwarts", "swim around", "defy gravity", "spy on the bad guys", "drive a car", "enter the rocket ship", "learn math", "write a lot", "do gymnastics"];
+var ScheduledActive = false;
+var ScheduledDisactive = false;
 
 /*
 	--------------------------
@@ -44,45 +29,8 @@ var Action = ["learn to read", "jump up and down", "cry a lot", "cry a little", 
     --------------------------
 */
 var Commands = {
-    "generate": function(type) {
-		if (type[0] == "prompt") {
-			postMessage("A story about a " + Adjective[randomNumber(0, Adjective.length - 1)] + " " + Noun[randomNumber(0, Noun.length - 1)] + " who must " + Goal[randomNumber(0, Goal.length - 1)] + " while " + Obstacle[randomNumber(0, Goal.length - 1)] + ".");
-		} else if (type[0] == "crawl") {
-			postMessage("You enter the " + Place_Adjective[randomNumber(0, Place_Adjective.length - 1)] + " " + Place[randomNumber(0, Place.length - 1)] + ". Write " + randomNumber(50, 500) + " words as you " + Action[randomNumber(0, Action.length - 1)] + ".");
-		} else {
-			postMessage("You can generate [b]prompt[/b]s and [b]crawl[/b] dares. Having trouble? Use ^help! :)");
-		}
-	},
 	"information": function() {
-		postMessage("Hello! I'm Talos, official PtP mod-bot.\nMy Developers are CraftSpider, Dino, and HiddenStorys.\nAny suggestions or bugs can be sent to my email, talos.ptp@gmail.com.");
-	},
-	"seen": function(user) {
-		if(user[0]) {
-			var time, iterations = 0;
-			user = user.join(" ");
-			searchMessages("", user);
-			var getTime = setInterval(function() {
-				if (elementsByClass(messageButton)) {
-					time = elementsByClass(messageTime)[0].innerText;
-					clearInterval(getTime);
-					setTimeout(function() {
-						closePopup();
-						postMessage("User " + user + " was last seen " + time);
-				    }, 500);
-				} else if (elementByID(popup).childNodes[0].innerText == "No Messages Found" || iterations > 60) {
-				    postMessage("I couldn't find " + user + ". Sorry.");
-				    clearInterval(getTime);
-				    setTimeout(function() {
-				        closePopup();
-				    }, 500);
-				} else {
-					iterations++;
-				}
-			}, 500);
-		} else {
-			postMessage("Sorry, I need a user to look for.");
-			closePopup();
-		}
+		postMessage("Hello! This is JSBot, a prototype Chatzy bot program.\nMy Developers are CraftSpider, Dino, and HiddenStorys.");
 	},
 	"uptime": function() {
 	    var uptime = Math.floor((new Date() - BOOT_TIME)/1000);
@@ -108,98 +56,9 @@ var Commands = {
 	"version": function() {
 		postMessage("I'm currently on version " + VERSION);
 	},
-	"roulette": function() {
-		var num = parseInt(Math.ceil(Math.random() * 6));
-		switch(num) {
-			case 1:
-				postMessage("Save the game 10 times, just to be sure");
-				break;
-			case 2:
-				postMessage("/me \u200B");
-				break;
-			case 3:
-				postMessage("How much you wanna bet wundr broke Talos deliberately?");
-				break;
-			case 4:
-				postMessage("This message has 50 characters in it\u200B\u200B\u200B\u200B\u200B\u200B\u200B\u200B\u200B\u200B\u200B\u200B\u200B\u200B");
-				break;
-			case 5:
-				postMessage("wundr's not good at making up dumb things for Talos to say, despite the fact that he himself speaks exclusively in stupid");
-				break;
-			case 6:
-				var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-				var arbitraryLetters = "";
-				
-				for(var i = 0; i < 10; i++) {
-					var index = parseInt(Math.floor(Math.random() * 26));
-					arbitraryLetters += alphabet[index];
-				}
-				
-				postMessage("Arbitrary string of ten letters: " + arbitraryLetters);
-				break;
-			default:
-				postMessage("pǝʞoɹq ɹ ᴉ | Go yell at wundrweapon");
-		}
-	},
-	"wordWar": function(args) {
-		if (args[0] > 60 || args[0] <= 0) {
-			postMessage("Choose a number between 1 and 60.");
-		} else if (NumWWs >= MaxWWs) {
-			postMessage("Too many word wars, I can't keep up! Wait for one to finish first.");
-		} else {
-			var length = args[0];
-			var StartTime;
-			var KeyWord;
-			
-			//Block of input handling. Checks for :xx format for times.
-			if(args[1] && args[2]) {
-				if(args[1].match(/:(\d\d)/)) {
-					StartTime = RegExp.$1;
-					KeyWord = args[2];
-				} else if (args[2].match(/:(\d\d)/)) {
-					StartTime = RegExp.$1;
-					KeyWord = args[1];
-				} else {
-					postMessage("I can't read that start time, sorry.");
-				}
-			} else if (args[1]) {
-				if(args[1].match(/:(\d\d)/)) {
-					StartTime = RegExp.$1;
-				} else {
-					KeyWord = args[1];
-				}
-			}
-			
-			//Yay for error handling.
-			if (StartTime && (StartTime > 59 || StartTime < 0)) {
-				postMessage("What part of the hour is that? Sorry, but I don't recognize that time.");
-			} else {
-			    var TimeDif;
-			    NumWWs++;
-			    
-				if (StartTime) {
-					//Figure out difference between StartTime and current time.
-					var CurTime = new Date();
-					var RawDif = StartTime - CurTime.getUTCMinutes();
-					RawDif = (RawDif >= 0 ? RawDif : RawDif + 60);
-					TimeDif = ((CurTime.getTime()-(CurTime.getSeconds()*1000+CurTime.getMilliseconds())) + RawDif * 60000) - CurTime.getTime();
-					postMessage("Alright, I'll start a " + length + " minute word war at :" + StartTime + "." + (KeyWord? " Keyword: " + KeyWord  + "." : ""));
-					console.log(length + " " + StartTime + " " + TimeDif);
-					setTimeout(function() {
-						postMessage("I'm starting the " + length + " minute word war." + (KeyWord? " Keyword: " + KeyWord  + "." : "") + " Go!");
-						console.log("I'm starting the " + length + " minute word war." + (KeyWord? " Keyword: " + KeyWord  + "." : "") + " Go!");
-						startWW(length, KeyWord);
-					}, TimeDif);
-				} else {
-					postMessage("I'm starting a " + length + " minute word war." + (KeyWord? " Keyword: " + KeyWord  + "." : "") + " Go!");
-					startWW(length, KeyWord);
-				}
-			}
-		}
-	},
 	"help": function (args) {
 	    if (!args[0]) {
-		    var helpList = "Greetings. I'm Talos, chat helper. My commands are:\n";
+		    var helpList = "Hello. I'm JSBot, Chatzy prototype bot. My commands are:\n";
 		    for (var C in Commands) {
 		    	if(C.toLowerCase() != "roulette") {
 		    		helpList += "^" + C + "\n";
@@ -215,17 +74,11 @@ var Commands = {
 	            case "help":
 	                postMessage("Use: ^help [Command Name]\nDescription: Help command, by default gives general information about Talos and a list of available commands. Adding the name of another command as an argument will give a more detailed description of that command. Though you probably figured that out, you're here after all :P");
 	                break;
-	            case "generate":
-	            	postMessage("Use: ^generate <type>\nDescription: Generates a prompt. Currently available types are [b]prompt[/b] and [b]crawl[/b].");
-	            	break;
 	            case "information":
 	            	postMessage("Use: ^information\nDescription: Gives a short blurb about Talos.");
 	            	break;
 	            case "kill":
 	                postMessage("Use: ^kill\nDescription: Causes Talos to immediately leave the chat, and cease running. Admin only.");
-	                break;
-	            case "seen":
-	                postMessage("Use: ^seen <Username>\nDescription: Find how long ago this user last posted a message. Returns a time or date in EST.");
 	                break;
 	            case "toggleSleep":
 	                postMessage("Use: ^toggleSleep [time]\nDescription: Turns user commands and related features off or on. An admin only command, to prevent abuse. Also doesn't declare the finish to any active WWs that finish while I'm asleep.");
@@ -236,9 +89,6 @@ var Commands = {
 	            case "version":
 	            	postMessage("Use: ^version\nDescription: The version that Talos is currently running. I always know exactly where I am.");
 	            	break;
-	            case "wordWar":
-	                postMessage("Use: ^wordWar <length> [keyword] [start time]\nDescription: Starts a Word War, with given keyword and start time if provided. Start time should be two digits, the minute of the hour that the WW should start, and be prefixed by a colon. The length is in minutes, and Talos will say when that many minutes have elapsed.");
-	                break;
 	            default:
 	                postMessage("Sorry, no available help page for that.");
 	        }
@@ -373,40 +223,23 @@ function randomNumber(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
 }
 
-function startWW(length, KeyWord) {
-	setTimeout(function() {
-		NumWWs--;
-		if (!IsSleeping) {
-			postMessage("Word War " + (KeyWord? "'" + KeyWord + "' " : "") + "ends. How did you do?");
-		}
-	}, length * 60000);
-}
-
 /*
     -------------------
     Main loop functions
     -------------------
 */
 
-function writingHour() {
+function scheduledEvent() {
     d = new Date();
     
-    if (d.getUTCHours() == (WH_TIME == 0 ? 23 : WH_TIME - 1)  && d.getUTCMinutes() == 50 && !WHAlertOne) {
-        postMessage("[b][Alert][/b] Writing Hour starts in 10 minutes!");
-        WHAlertOne = true;
-    } else if (d.getUTCHours() == (WH_TIME == 0 ? 23 : WH_TIME - 1) && d.getUTCMinutes() == 55 && !WHAlertTwo) {
-        postMessage("[b][Alert][/b] Writing Hour starts in 5 minutes!");
-        WHAlertTwo = true;
-    } else if (d.getUTCHours() == WH_TIME && d.getUTCMinutes() == 0 && !WHActive) {
-        postMessage("[b]Writing Hour has started.[/b] Have fun, and use it productively!");
-        WHActive = true;
-        WHDisactive = false;
-    } else if (d.getUTCHours() == (WH_TIME == 23 ? 0 : WH_TIME + 1) && d.getUTCMinutes() == 0 && !WHDisactive) {
-        setTimeout(function() {postMessage("[b]Writing Hour is over.[/b] How did you do?");}, 500);
-        WHDisactive = true;
-        WHActive = false;
-        WHAlertTwo = false;
-        WHAlertOne = false;
+    if (d.getUTCHours() == SCHEDULED_TIME && d.getUTCMinutes() == 0 && !ScheduledActive) {
+        postMessage("[b]Scheduled Event[/b] has begun!");
+        ScheduledActive = true;
+        ScheduledDisactive = false;
+    } else if (d.getUTCHours() == (SCHEDULED_TIME == 23 ? 0 : SCHEDULED_TIME + 1) && d.getUTCMinutes() == 0 && !ScheduledDisactive) {
+        postMessage("[b]Scheduled Event[/b] is over!");
+        ScheduledDisactive = true;
+        ScheduledActive = false;
     }
 }
 
@@ -477,7 +310,7 @@ function readPMs() {
 
 function mainLoop() {
     readChat();
-    writingHour();
+    scheduledEvent();
     readPMs();
     
 }
